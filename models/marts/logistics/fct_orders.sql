913245367f5fff97e1c3ef4d8c589ccfc4b8aaaa
aceino <marcelinokamasi76@gmail.com>
2026-09-16 20:03:18 +0700

    updating

--- models/marts/logistics/fct_orders.sql
+++ models/marts/logistics/fct_orders.sql
@@ -10,7 +10,6 @@
     date_diff('day', purchased_at, delivered_to_customer_at) as fulfillment_days, 
     case when delivered_to_customer_at <= estimated_at then 1 else 0 end as delivered_on_time
 
-
 from {{ ref ('stg_orders') }}
 where delivered_to_customer_at is not null 
 
